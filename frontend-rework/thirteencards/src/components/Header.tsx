60dfa37a8ba4bcb907a72996bb25cc17feb984a5
calvinKLyeung <89771166+calvinKLyeung@users.noreply.github.com>
2026-09-01 21:09:15 -0700

    update Header, index.html, CSS

--- frontend-rework/thirteencards/src/components/Header.tsx
+++ frontend-rework/thirteencards/src/components/Header.tsx
@@ -1,12 +1,9 @@
 function Header() {
 
     return (
-        <>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <h1>Thirteen</h1>
-            </div>
-
-        </>
+        <header className="site-header">
+            <span className="wordmark">Thirteen</span>
+        </header>
     )
 }
 
